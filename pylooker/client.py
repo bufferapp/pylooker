f773815689f27595e8b82d7eb98080e4c643e591
David Gasquez <davidgasquez@gmail.com>
2017-04-28 12:30:21 +0200

    First version

---
+++ pylooker/client.py
@@ -0,0 +1,105 @@
+import requests
+
+
+class LookerClient(object):
+    """Client for Looker API.
+
+    Used to initialize the client id and secret.
+
+    Parameters
+    ----------
+    api_endpoint : str
+        The API endpoint implemented for your Looker instance. Usually looks
+        like https://<your-looker-host-name>:19999/api/3.0/
+    client_id : str
+        Client ID from the API3 credentials
+    client_secret : str
+        Client Secret from the API3 credentials
+
+    Usage
+    -----
+    >>> from pylooker.client import LookerClient
+    >>> api_endpoint = 'https://looker.company.com:19999/api/3.0/'
+    >>> client_id = 'your-client-id'
+    >>> client_secret = 'your-client-secret'
+    >>> lc = LookerClient(api_endpoint, client_id, client_secret)
+    """
+    def __init__(self, api_endpoint, client_id, client_secret):
+        self.api_endpoint = api_endpoint
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def _get_headers(self):
+        token_request = requests.post(
+            url=self.api_endpoint+'login',
+            data={
+                'client_id': self.client_id,
+                'client_secret': self.client_secret
+            }
+        )
+
+        token = token_request.json()['access_token']
+        headers = {'Authorization': 'token ' + token}
+
+        return headers
+
+    def run_look(self, look_id, format='json', limit=-1):
+        """Get the result of running the specified look.
+
+        Parameters
+        ----------
+        look_id : int
+            Identifier of the saved look
+        format : str
+            Response format
+        limit : int
+            Limit the number of rows returned. Defaults to -1 to return all the
+            results
+        """
+        headers = self._get_headers()
+
+        response = requests.get(
+            url='{}{}/run/{}?limit={}'.format(
+                self.api_endpoint + 'looks/',
+                look_id,
+                format,
+                limit
+            ),
+            headers=headers
+        )
+
+        return response.json()
+
+    def run_query(self, slug, format='json', limit=-1):
+        """Get the result of running the specified query.
+
+        Parameters
+        ----------
+        slug : str
+            Identifier of the query. This slug is in every query URL after the
+            qid parameter. In short URLs, the slug is the latest part of the
+            URL
+        format : str
+            Response format
+        limit : int
+            Limit the number of rows returned. Defaults to -1 to return all the
+            results
+        """
+        headers = self._get_headers()
+
+        slug_response = requests.get(
+            url=self.api_endpoint + '/queries/slug/' + slug,
+            headers=headers
+        )
+
+        response = requests.get(
+            url='{}{}/run/{}?limit={}'.format(
+                self.api_endpoint + 'queries/',
+                slug_response.json()['id'],
+                format,
+                limit
+            ),
+            headers=headers
+        )
+
+        return response.json()
---
+++ setup.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pylooker',
+    packages=[],
+    version='0.1.0',
+    description='A Python interface to Looker API',
+    author='David Gasquez',
+    license='MIT',
+    author_email='davidgasquez@gmail.com',
+    url='https://github.com/bufferapp/pylooker',
+    keywords=['looker'],
+)
